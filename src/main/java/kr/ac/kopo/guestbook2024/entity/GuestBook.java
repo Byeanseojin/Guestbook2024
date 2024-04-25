c8bfa0f7c6cafd38661a80963e3a611d70ee76cf
Byeanseojin <sjb12365@gmail.com>
2024-04-25 09:50:52 +0900

    Guestbook entity

---
+++ src/main/java/kr/ac/kopo/guestbook2024/entity/GuestBook.java
@@ -0,0 +1,28 @@
+package kr.ac.kopo.guestbook2024.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Entity
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+public class GuestBook extends BaseEntity{
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long gno;
+
+    @Column(length = 100, nullable = false)
+    private String title;
+
+    @Column(length = 1500, nullable = false)
+    private String content;
+
+    @Column(length = 50, nullable = false)
+    private String writer;
+
+}
