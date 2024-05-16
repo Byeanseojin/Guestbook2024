0ad534d3dc3c7c754df3125bed1c4955d1a91ce7
Byeanseojin <sjb12365@gmail.com>
2024-05-16 11:48:27 +0900

    서비스계층 추가 및 목록처리에 필요한 정보 저장, 테스트

--- src/main/java/kr/ac/kopo/guestbook2024/service/GuestbookService.java
+++ src/main/java/kr/ac/kopo/guestbook2024/service/GuestbookService.java
@@ -1,12 +1,18 @@
 package kr.ac.kopo.guestbook2024.service;
 
 import kr.ac.kopo.guestbook2024.dto.GuestbookDTO;
+import kr.ac.kopo.guestbook2024.dto.PageRequestDTO;
+import kr.ac.kopo.guestbook2024.dto.PageResultDTO;
 import kr.ac.kopo.guestbook2024.entity.Guestbook;
 
 public interface GuestbookService {
+    //글등록 가능
     Long register(GuestbookDTO dto);
 
-    default Guestbook dtoToEntity(GuestbookDTO dto){
+    //한 페이지에 보여질 글 목록(GuestbookDTO 객체)이 저장된 list정보를 갖고 있는 PageResultDTO객체 참조값을 반환하는 기능)
+    PageResultDTO<GuestbookDTO, Guestbook> getList(PageRequestDTO resultDTO);
+
+    default Guestbook dtoToEntity(GuestbookDTO dto) {
         Guestbook entity = Guestbook.builder()
                 .gno(dto.getGno())
                 .title(dto.getTitle())
@@ -14,5 +20,17 @@ public interface GuestbookService {
                 .writer(dto.getWriter())
                 .build();
         return entity;
+    }
+
+    default GuestbookDTO entityToDTO(Guestbook entity) {
+        GuestbookDTO dto = GuestbookDTO.builder()
+                .gno(entity.getGno())
+                .title(entity.getTitle())
+                .content(entity.getContent())
+                .writer(entity.getWriter())
+                .regDate(entity.getRegDate())
+                .modDate(entity.getModDate())
+                .build();
+        return dto;
     }
 }
--- src/main/java/kr/ac/kopo/guestbook2024/service/GuestbookServiceImpl.java
+++ src/main/java/kr/ac/kopo/guestbook2024/service/GuestbookServiceImpl.java
@@ -1,10 +1,17 @@
 package kr.ac.kopo.guestbook2024.service;
 
 import kr.ac.kopo.guestbook2024.dto.GuestbookDTO;
+import kr.ac.kopo.guestbook2024.dto.PageRequestDTO;
+import kr.ac.kopo.guestbook2024.dto.PageResultDTO;
 import kr.ac.kopo.guestbook2024.entity.Guestbook;
 import kr.ac.kopo.guestbook2024.repository.GuestbookRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
+
+import java.util.function.Function;
 
 @Service
 @RequiredArgsConstructor
@@ -17,5 +24,18 @@ public class GuestbookServiceImpl implements GuestbookService{
 
         repository.save(entity);
         return entity.getGno();
+    }
+
+    @Override
+    public Guestbook dtoToEntity(GuestbookDTO dto) {
+        return GuestbookService.super.dtoToEntity(dto);
+    }
+
+    @Override
+    public PageResultDTO<GuestbookDTO, Guestbook> getList(PageRequestDTO requestDTO) {
+        Pageable pageable = requestDTO.getPageable(Sort.by("gno").descending());
+        Page<Guestbook> result = repository.findAll(pageable);
+        Function<Guestbook, GuestbookDTO> fn = (entity -> entityToDTO(entity));
+        return new PageResultDTO<>(result,fn);
     }
 }
